8c89bff21f9f018d3beb9a6630f33de1dbf62e02
Yongnam Park <devnoff@gmail.com>
2013-01-28 19:45:58 +0900

    회사소개 디자인 변경

--- ko/application/views/shop/etc_company_view.php
+++ ko/application/views/shop/etc_company_view.php
@@ -1,12 +1,14 @@
 		<h4>회사소개</h4>
 		<div class="about">
-			<p><strong>CultStory</strong>는 온라인 및 모바일 쇼핑 플랫폼을 바탕으로<br/> 스마트폰, SNS 시대에 최적화된 쇼핑 환경을 제공하고 있는 회사입니다..</p>
+			<p style="text-align:center"><img src="/ko/img/cultstory_casebuy.png"/></p>
+			<p style="text-align:center"><strong>CultStory</strong>는 온라인 및 모바일 쇼핑 플랫폼을 바탕으로<br/> 스마트폰, SNS 시대에 최적화된 쇼핑 환경을 제공하고 있는 회사입니다..</p>
 
 			<ul>
 				<li><strong>주소</strong> 서울시 중구 충무로2가 50-6 라이온스빌딩 1003호</li>
 				<li><strong>연락처</strong> TEL 070-8280-4090</li>
 				<li><strong>FAX</strong> 02-6280-7428</li>
 				<li><strong>Email</strong> casebuy@cultstory.com</li>
+				<li><strong>Website</strong> <a href="http://cultstory.com" target="_blank">http://cultstory.com</a></li>
 			</ul>
 
 
